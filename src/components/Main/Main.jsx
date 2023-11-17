1208c9a4930ab3817cba7a4633c12ba8d7999dc4
Tilek-Dzhenishev <tilek.dzenisev@gmail.com>
2023-11-18 00:17:16 +0100

    Main Page done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import style from "./Main.scss";
 import { TypeAnimation } from "react-type-animation";
+import sticker from "../../assets/sticker.png";
 
 function Main() {
   return (
     <div>
       <div className="container__main">
-        <div className="container__type"></div>
-        <div className="container__image">
+        <div className="container__type">
           <div className="typeanimation">
-            <h2>I am</h2>
+            <h2 className="title_of_type">Hello, I am</h2>
 
             <TypeAnimation
+              className="type"
               sequence={[
                 "Tilek",
                 2000,
                 "Software engineer",
                 2000,
-                "Designer",
+                "UX/UI Designer",
                 2000,
-                "Inventor",
+                "Web developer",
                 2000,
                 () => {
                   console.log("Sequence completed");
@@ -28,9 +29,20 @@ function Main() {
               wrapper="span"
               cursor={true}
               repeat={Infinity}
-              style={{ fontSize: "5em", display: "inline-block", color: "white"}}
+              style={{
+                display: "inline-block",
+                fontFamily: "sans-serif",
+                fontWeght: "bold",
+              }}
             />
           </div>
+          <div className="container_img">
+            <img className="img" src={sticker} />
+          </div>
+        </div>
+        <div className="buttons">
+          <button className="buttons__first">Hire me</button>
+          <button className="buttons__second">Download CV</button>
         </div>
       </div>
     </div>
